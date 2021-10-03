5a3a02f250c52a89d14b1a9440534ebade6245b3
Redhart Azul <me@dystopian.dev>
2021-10-03 20:04:55 +0100

    Update ubuntu_20.04_add_ctx_user_to_docker_group.sh

--- installation/docker/ubuntu_20.04_add_ctx_user_to_docker_group.sh
+++ installation/docker/ubuntu_20.04_add_ctx_user_to_docker_group.sh
@@ -1,6 +1,10 @@
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-sudo apt install docker-ce
-sudo usermod -aG docker ${USER}
+sudo apt install -y docker-ce
     
+# Show installed version
+sudo docker --version
+
+# Add current user to docker group (takes effect on new session)
+sudo usermod -aG docker ${USER}
